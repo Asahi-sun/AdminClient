8a28f871ac25a5a85b57070cfb36a7379b9f4aa1
赵旭东 <1596014525@qq.com>
2020-08-18 19:00:24 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/**
+ * 应用根组件
+ */
+import React from 'react'
+
+ class App extends React.Component{
+
+    render(){
+        return(
+            <div>
+                App
+            </div>
+        )
+    }
+ }
+
+ export default App
